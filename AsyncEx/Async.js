f5a9e1c0ec27df113ef5a8d1e540a74384ff315a
Naftali Berger <bn26665@gmail.com>
2025-06-30 17:57:38 +0300

    added ex3 , ex4 ,ex5

--- AsyncEx/Async.js
+++ AsyncEx/Async.js
@@ -1,5 +1,6 @@
 //ex1
 import fs from "fs";
+import os from "os";
 // fs.readFile("./file1.txt","utf8",(err,data)=>{
 //     if(err){
 //         console.log(err);
@@ -30,4 +31,49 @@ import fs from "fs";
 
 //ex3
 
+// function connt(n){
+//     console.log(n);
+//     if(n == 0){
+//         console.log("end");
+//         return;  
+//     }
+//     n -= 1
+//     setTimeout(()=>{
+//         connt(n)
+//     },1000)
+// }
+// connt(5)
 
+
+//ex4
+// fs.readFile("./file1.txt", "utf8", (err, data) => {
+//     if (err) {
+//         return console.log(err);
+//     }
+//     const lines = data.split("\n").filter(line => line);
+//     const randomIndex = Math.floor(Math.random() * lines.length);
+//     const randomLine = lines[randomIndex];
+
+//     console.log("Random line:", randomLine);
+// });
+
+//ex5
+
+let count = 0;
+
+
+const intervalId = setInterval(() => {
+  const freeMemBytes = os.freemem();
+  const totalMemBytes = os.totalmem();
+  const freeMemPercentage = ((freeMemBytes / totalMemBytes) * 100).toFixed(2);
+
+  console.log(`Free Memory: ${freeMemPercentage}%`);
+
+  count++;
+  if (count >= 5) {
+    clearInterval(intervalId);
+    console.log("Memory logging complete.");
+  }
+}, 2000);
+
+  
