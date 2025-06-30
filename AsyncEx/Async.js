350e6a030bcdaff195996856dd47c39427a22eca
Naftali Berger <bn26665@gmail.com>
2025-06-30 15:42:16 +0300

    added ex1and ex2

--- AsyncEx/Async.js
+++ AsyncEx/Async.js
@@ -1,2 +1,33 @@
+//ex1
 import fs from "fs";
+// fs.readFile("./file1.txt","utf8",(err,data)=>{
+//     if(err){
+//         console.log(err);
+//         return;
+//     }
+//     console.log(data);
+//     console.log(data.length);
+
+//     fs.stat("./file1.txt","utf8",(err,stats)=>{
+//         if(err){
+//             console.log(err);
+//             return;
+//         }
+       
+//         console.log(stats.birthtime);
+//     });
+// });
+
+//ex2
+// fs.readdir("./",(err,data)=>{
+//     if(err){
+//         console.log(err);
+//         return
+//     }
+//     console.log(data);
+    
+// });
+
+//ex3
+
 
